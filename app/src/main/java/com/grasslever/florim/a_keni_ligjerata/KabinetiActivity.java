8bd4e00eae0e72181fadb77badb1eeaf97b82ebe
Gent Rexha <gent.rexha@gmail.com>
2017-08-01 14:02:49 +0200

    Added a cool profile layout placeholder :fire:

--- app/src/main/java/com/grasslever/florim/a_keni_ligjerata/KabinetiActivity.java
+++ app/src/main/java/com/grasslever/florim/a_keni_ligjerata/KabinetiActivity.java
@@ -2,6 +2,10 @@ package com.grasslever.florim.a_keni_ligjerata;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.vansuita.materialabout.builder.AboutBuilder;
+import com.vansuita.materialabout.views.AboutView;
 
 //import com.grasslever.florim.a_keni_ligjerata.R;
 
@@ -10,6 +14,26 @@ public class KabinetiActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_kabineti);
+
+        AboutView view = AboutBuilder.with(this)
+                .setPhoto(R.mipmap.profile_picture)
+                .setCover(R.mipmap.profile_cover)
+                .setName("Your Full Name")
+                .setSubTitle("Mobile Developer")
+                .setBrief("I'm warmed of mobile technologies. Ideas maker, curious and nature lover.")
+                .setAppIcon(R.mipmap.ic_launcher)
+                .setAppName(R.string.app_name)
+                .addGooglePlayStoreLink("8002078663318221363")
+                .addGitHubLink("user")
+                .addFacebookLink("user")
+                .addFiveStarsAction()
+                .setVersionNameAsAppSubTitle()
+                .addShareAction(R.string.app_name)
+                .setWrapScrollView(true)
+                .setLinksAnimated(true)
+                .setShowAsCard(true)
+                .build();
+
+        addContentView(view, view.getLayoutParams());
     }
 }
